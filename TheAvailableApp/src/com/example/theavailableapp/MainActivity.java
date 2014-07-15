f0b4ebc20bec0981fdafdc6a303d887a4a9f8198
Stephanie Choi <steph.choi92@gmail.com>
2014-07-15 16:13:15 -0700

    Fixing errors

---
+++ TheAvailableApp/src/com/example/theavailableapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.theavailableapp;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	//Google Map
+		private GoogleMap googleMap;
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_main);
+		
+		try {
+			// Loading map
+			initilizeMap();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
+	
+		
+		private void initilizeMap() {
+	        if (googleMap == null) {
+	        	googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	         
+
+	            // check if map is created successfully or not
+	            if (googleMap == null) {
+	                Toast.makeText(getApplicationContext(),
+	                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+	                        .show();
+	            }
+	        }
+		}
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.main, menu);
+			return true;
+		}
+
+	}
